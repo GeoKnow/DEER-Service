d2fff4278792bef6da8cb4e3e08d8973ba1b1d93
jj <jj@TheFarm>
2013-11-29 12:50:19 +0100

    Latest version

---
+++ src/main/java/org/linkeddata/stack/service/geolift/GeoLiftReview.java
@@ -0,0 +1,55 @@
+package org.linkeddata.stack.service.geolift;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFReader;
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class TripleGeoReview
+ */
+public class GeoLiftReview extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private static String filePath;
+	
+	// Options for writing the config file
+	static String configFile;
+	static String configTemplate;
+	static String outputFormat;
+	static String execType;
+	   
+
+	   public void init( ){
+		   filePath = getServletContext().getRealPath(File.separator);
+	   }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	 Model mod0 = ModelFactory.createDefaultModel();
+
+	   	 RDFReader r0 = mod0.getReader( "N3" );
+	   	 r0.read( mod0, "file:///"+filePath+"result/result.ttl" );
+	   	 
+	   	 //mod0.add(mod1);
+	   	 String[] modArray = new String[2];
+	   	 modArray[0] = mod0.toString();
+	     
+	     Gson gson = new Gson();
+	     String json = gson.toJson(modArray);
+	     response.setContentType("application/json");
+	     response.setCharacterEncoding("UTF-8");
+	     response.getWriter().write(json);
+    	}
+}
---
+++ src/main/java/org/linkeddata/stack/service/geolift/GeoLiftRun.java
@@ -0,0 +1,107 @@
+package org.linkeddata.stack.service.geolift;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.aksw.geolift.workflow.GeoLift;
+
+/**
+ * Servlet implementation class TripleGeoRun
+ */
+public class GeoLiftRun extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private static String filePath;
+	static String[]args = new String[6];
+	
+	// Options for writing the config file
+	static String configFile;
+	static String outputFile;
+
+	   public void init( ){
+		  filePath = getServletContext().getRealPath(File.separator);
+	      configFile = filePath+"config/config.tsv";
+	      outputFile = filePath+"result/result.ttl";
+	      args[0] = "-i";
+	  	  args[2] = "-c";
+	      args[3] = configFile;
+	      args[4] = "-o";
+	      args[5] = outputFile;
+	   }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML><HEAD><TITLE>Hello World!</TITLE>"
+		+ "</HEAD><BODY>Hello World!!!</BODY></HTML>");
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	String strlen = request.getParameter("0");
+    	int length = (Integer.parseInt(strlen)+3);
+    	
+    	String [] params = new String[length];
+    	
+    	for(int i=3; i<length; i++){
+	    	params[i-3] = request.getParameter(Integer.toString(i));
+	    	System.out.println(params[i-3]);
+    	}
+    	
+    	String input = request.getParameter("1");
+    	int isCompletePath = Integer.parseInt(request.getParameter("2"));
+    	
+    	if(isCompletePath == 0){
+    		args[1] = filePath+"examples"+File.separator+input;
+    	}else{
+    		args[1] = input;
+    	}
+    	
+    	try {
+ 
+			File file = new File(configFile);
+			
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			for(int i=0; i<length-3; i++){
+		    		String line = params[i];
+		    		bw.write(line);
+		        	bw.newLine();
+		    }
+			bw.close();
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	ExecuteGeoLift(args);
+    	response.getWriter().write("Finished!");
+    }
+    
+    // Start GeoLift with the configfile
+ 	public static void ExecuteGeoLift(String args[]) throws IOException{
+ 		System.out.println(args[1]);
+ 		GeoLift.main(args);
+ 	}
+
+}
